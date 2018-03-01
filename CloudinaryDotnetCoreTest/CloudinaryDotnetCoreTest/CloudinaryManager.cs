cdeb046b5baa4e6ad2465112e3e8f8edddb6a621
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-03-01 15:42:10 +0200

    add cloudinaryManager

---
+++ CloudinaryDotnetCoreTest/CloudinaryDotnetCoreTest/CloudinaryManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace CloudinaryDotnetCoreTest
+{
+    public static class CloudinaryManager
+    {
+        #region VAR
+        private static Account account = new Account(
+                         "tandtapp",
+                         "959336366472599",
+                         "goS8W-C-4tOCvfe8ixl8pGSgirY");
+
+        private static Cloudinary cloudinary = new Cloudinary(account);
+        #endregion
+
+        #region UPLOAD
+
+        /// <summary>
+        /// Завантажує фотографію на Cloudinary 
+        /// </summary>
+        /// <param name="stream"> Фото</param>
+        /// <returns></returns>
+        public static async Task<string> UploadPhoto(Stream stream)
+        {
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription("test", stream ),
+                Transformation = new Transformation().Crop("limit").Width(800).Height(800).FetchFormat("png"),
+               
+            };
+            var uploadResult = await cloudinary.UploadAsync(uploadParams);
+
+            return uploadResult.Uri.AbsoluteUri;
+        }
+
+        /// <summary>
+        /// Завантажує фотографію на Cloudinary 
+        /// </summary>
+        /// <param name="stream"> Фото </param>
+        /// <returns></returns>
+        public static async Task<string> UploadAvatar(Stream stream)
+        {
+            try
+            {
+                var uploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription("test", stream),
+                    Transformation = new Transformation().Gravity("face").Width(200).Height(200).Crop("thumb").Radius("max"),
+
+                };
+                var uploadResult = await cloudinary.UploadAsync(uploadParams);
+
+                return uploadResult.Uri.AbsoluteUri;
+            }
+        }
+        
+        #endregion
+    }
+}
--- CloudinaryDotnetCoreTest/CloudinaryDotnetCoreTest/Program.cs
+++ CloudinaryDotnetCoreTest/CloudinaryDotnetCoreTest/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace CloudinaryDotnetCoreTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void  Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string pathSource = @"F:\lady.jpg";
+            using (FileStream fsSource = new FileStream(pathSource,
+            FileMode.Open, FileAccess.Read))
+            {
+                Console.WriteLine(CloudinaryManager.UploadAvatar(fsSource).GetAwaiter().GetResult());
+            }
+            Console.Read();
         }
     }
 }
