4bd6da5f2df0c6296509e360c1a137c7cdd22f9a
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-04-02 20:41:50 +0300

    Add PointService

--- MinSpanningTree/MinSpanningTree/MainWindow.xaml.cs
+++ MinSpanningTree/MinSpanningTree/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using OxyPlot;
+﻿using MinSpanningTree.Services;
+using OxyPlot;
 using OxyPlot.Series;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace MinSpanningTree.UI
@@ -17,23 +19,91 @@ namespace MinSpanningTree.UI
     {
         public string Title { get; private set; }
         public List< List<DataPoint> > Lines { get; private set; }
+        public HashSet<DataPoint> Points { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            
             Lines = new List<List<DataPoint>>();
-
-            AddLines();
+            Points = PointService.GetPoints();
+            ReloadCommand(null,null);
         }
 
-        public void AddLines()
+        private void AddLines()
         {
             foreach (var cur in Lines)
             {
                 var line = new OxyPlot.Wpf.LineSeries();
-                line.ItemsSource = cur; 
+
+                line.ItemsSource = cur;
+                var about = "";
+                foreach (var i in cur)
+                {
+                    about += $"({i.X},{i.Y})";
+                }
+                line.TrackerFormatString = about;
                 plot.Series.Add(line);
             }
         }
+
+        private void NewLine(DataPoint point1, DataPoint point2)
+        {
+            var line = new List<DataPoint>();
+            line.Add(point1);
+            line.Add(point2);
+        }
+
+        private void NewLine(double x1, double y1,double x2, double y2)
+        {
+            var point1 = new DataPoint(x1, y1);
+            var point2 = new DataPoint(x2, y2);
+            NewLine(point1, point2);
+        }
+
+        public void AddPointCommand(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var _x = Convert.ToDouble(x.Text);
+                var _y = Convert.ToDouble(y.Text);
+                if (Points.Add(new DataPoint(_x, _y)))
+                    MessageBox.Show($"Точку ({_x}, {_y}) додано");
+                else MessageBox.Show($"Точка ({_x}, {_y}) існує");
+                x.Text = "";
+                y.Text = "";
+            }
+            catch { }
+        }
+
+        public void AddRandCommand(object sender, RoutedEventArgs e)
+        {
+            if (Points == null) Points = new HashSet<DataPoint>();
+            Random random = new Random();
+            var _x = random.NextDouble()* random.Next(50);
+            var _y = random.NextDouble() * random.Next(50);
+            if (Points.Add(new DataPoint(_x, _y)))
+                MessageBox.Show($"Точку ({_x}, {_y}) додано");
+            else AddRandCommand(sender, e);
+
+        }
+
+        public void SaveCommand(object sender, RoutedEventArgs e)
+        {
+            PointService.Save(Points);
+        }
+
+        public void ReloadCommand(object sender, RoutedEventArgs e)
+        {
+            Lines.Clear();
+
+            var list = new List<DataPoint>();
+            list.Add(new DataPoint(0, 0));
+            list.Add(new DataPoint(1, 2));
+            Lines.Add(list);
+            AddLines();
+            
+        }
+
     }
 }
---
+++ MinSpanningTree/MinSpanningTree/Services/PointService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Resources;
+using System.IO;
+
+namespace MinSpanningTree.Services
+{
+    public class PointService
+    {
+        private static string path = @"F:\VS\.NET\MinSpanningTree\MinSpanningTree\Resources\Points.txt";
+
+        public static void Save(HashSet<DataPoint> points)
+        {
+            string json = JsonConvert.SerializeObject(points);
+                using (var ct = File.CreateText(path))
+                {
+                    ct.Write(json);
+                }
+            
+        }
+        public static HashSet<DataPoint> GetPoints()
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<HashSet<DataPoint>>(json) ?? new HashSet<DataPoint>();
+        }
+    }
+}
